26966018b0a6d0007b5a22b4b35e5cb3d66ad7ee
Mate <mic24@kent.ac.uk>
2021-06-18 13:19:03 +0100

    adjusted tests to accomodate the longer output if line_type=True

--- tests/test_file.py
+++ tests/test_file.py
@@ -21,13 +21,13 @@ def test_tuple_length():
 @pytest.mark.length
 def test_return_tuple():
     answer = find_points((5,-25),(40,0),5)
-    assert len(answer) == 10
+    assert len(answer) == 10 or len(answer) == 16
 
 @pytest.mark.length
 def test_return_coordinate_check():
     answer = find_points((5,-25),(40,0),5)
     for coord in answer:
-        assert len(coord) == 2
+        assert len(coord) == 2 or len(coord) == 3
 
 @pytest.mark.value
 def test_returned_values_for_coordinates():
