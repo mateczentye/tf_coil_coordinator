fb5bf3a1759f2900638bb55cb1aa7d54989ccd52
Mate <mic24@kent.ac.uk>
2021-06-23 15:42:56 +0100

    testing ways to skip paramak module tests on CI

--- tests/test_paramak.py
+++ tests/test_paramak.py
@@ -5,7 +5,7 @@ from tf_coil_coordinator import find_points, surface_area, volume
 
 pytest.importorskip('paramak')
 
-@pytest.mark.value
+@pytest.mark.skip
 def test_surface_area():
     paramak_area = ExtrudeMixedShape(
         points = find_points((50, 0), (100, 100), 20, line_type=True),
@@ -13,7 +13,7 @@ def test_surface_area():
     package_area = surface_area((50, 0),(100, 100), 20, 10)
     assert pytest.approx(package_area) == paramak_area
 
-@pytest.mark.value
+@pytest.mark.skip
 def test_volume():
     paramak_vol = ExtrudeMixedShape(
         points = find_points((50, 0), (100, 100), 20, line_type=True),
